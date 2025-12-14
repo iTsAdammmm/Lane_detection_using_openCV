2f39e3942d4255fe2a12ab1a4c79a15b84bdd2e4
iTsAdammmm <adamash1205@gmail.com>
2025-12-14 10:14:19 +0530

    Added Canny edge detection and ROI masking

--- src/lane_detection.py
+++ src/lane_detection.py
@@ -1,4 +1,11 @@
 import cv2
+import numpy as np
+
+def region_of_interest(img,vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask,vertices,255)
+    roi = cv2.bitwise_and(img,mask)
+    return roi
 
 # Load video
 video = cv2.VideoCapture("../assets/road.mp4")
@@ -18,8 +25,17 @@ while video.isOpened():
     # Apply Gaussian Blur
     blur = cv2.GaussianBlur(gray, (5,5), 0)
 
+    edges = cv2.Canny(blur,50,150)
+    height, width = edges.shape[:2]
+    roi_vertices = np.array([[
+        (0,height),
+        (width, height),
+        (width // 2, height // 2)
+    ]], dtype=np.int32)
+    cropped_edges = region_of_interest(edges, roi_vertices)
+
     # Show output
-    cv2.imshow("Lane Detection - Step 1", blur)
+    cv2.imshow("Lane Detection - ROI Edges", cropped_edges)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
