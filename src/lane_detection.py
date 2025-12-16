dc117b4f1c9f09d91dd77c23e27d797aad209d3f
iTsAdammmm <adamash1205@gmail.com>
2025-12-16 17:44:44 +0530

    Added Hough line transform for lane detection

--- src/lane_detection.py
+++ src/lane_detection.py
@@ -7,6 +7,17 @@ def region_of_interest(img,vertices):
     roi = cv2.bitwise_and(img,mask)
     return roi
 
+def draw_lines(img, lines, color=(0,255,0), thickness=5):
+    if lines is None:
+        return img
+    line_img = np.zeros_like(img)
+
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(line_img, (x1,y1), (x2,y2), color, thickness)
+    
+    return cv2.addWeighted(img, 0.8, line_img, 1, 0)
+
 # Load video
 video = cv2.VideoCapture("../assets/road.mp4")
 
@@ -26,16 +37,29 @@ while video.isOpened():
     blur = cv2.GaussianBlur(gray, (5,5), 0)
 
     edges = cv2.Canny(blur,50,150)
+
     height, width = edges.shape[:2]
     roi_vertices = np.array([[
         (0,height),
         (width, height),
         (width // 2, height // 2)
     ]], dtype=np.int32)
+    
     cropped_edges = region_of_interest(edges, roi_vertices)
 
+    lines = cv2.HoughLinesP(
+        cropped_edges,
+        rho = 1,
+        theta = np.pi / 180,
+        threshold = 50,
+        minLineLength = 100,
+        maxLineGap = 50
+    )
+
+    lane_frame = draw_lines(frame, lines)
+
     # Show output
-    cv2.imshow("Lane Detection - ROI Edges", cropped_edges)
+    cv2.imshow("Lane Detection - Lines", lane_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
